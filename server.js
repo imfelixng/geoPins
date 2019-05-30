22e6e02dafd97f4fdc6fac6d807bdf86dd39370b
Nguyen Quang An <ngquangan@gmail.com>
2019-05-30 15:02:02 +0700

    Connect to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+require('dotenv').config({ path: '.env' })
 const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
 
 const typeDefs = require('./typeDefs');
 
@@ -8,5 +10,9 @@ const server = new ApolloServer({
   typeDefs,
   resolvers
 });
+
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true})
+  .then(() => console.log("Connect mongodb successfully"))
+  .catch(err => console.log(err));
 
 server.listen().then(({url}) => console.log(url));
