7fe9fba0058186ab3aec23d26a9f136f85276ef7
Nguyen Quang An <ngquangan@gmail.com>
2019-05-31 23:16:34 +0700

    Add mapbox

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 16.05632,
+  longitude: 108.1786368,
+  zoom: 8,
+}
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className = { classes.root }>
+      <ReactMapGL
+        onViewportChange = { newViewport => { setViewport(newViewport) } }
+        { ...viewport }
+        width = "100vw"
+        height="calc(100vh - 64px)"
+        mapStyle = "mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken = "pk.eyJ1IjoibmdxdWFuZ2FuIiwiYSI6ImNqdGpvazdqbTBhZmczeXFqd242dHE3a2YifQ.x8OrqwQl6qptF7I15GwHfQ"
+      >
+        <div className = { classes.navigationControl }>
+          <NavigationControl 
+            onViewportChange = { newViewport => { setViewport(newViewport) } }
+          />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
